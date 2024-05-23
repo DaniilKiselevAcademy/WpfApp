2f060cf0d51351845b03dac961e1bfd31fcc0186
Daniil <kiselev.daniil.academy@gmail.com>
2024-05-24 02:21:35 +0300

    Добавлен контекст и модель студентов

---
+++ WpfApp/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WpfApp
+{
+    class Context : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=test.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student { StudentId = 1, Name = "Марк", Age=16 },
+                new Student { StudentId = 2, Name = "Денис", Age=16 }
+                ); 
+        }
+    }
+}
---
+++ WpfApp/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WpfApp
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
